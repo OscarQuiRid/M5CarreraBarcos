029e645da96d489aed67b5266d62f0ff667e9e38
OscarQuiRid <153615994+OscarQuiRid@users.noreply.github.com>
2024-05-02 17:46:14 +0200

    iniciando eleccion barco para enemigos

--- Main.cpp
+++ Main.cpp
@@ -73,8 +73,9 @@ int main() {
 	Barcos playerUno;
 	Barcos cargador;			// variable para cargar funciones
 	string eleccion;
+	bool playerUnoIniciar, enemyUnoIniciar, enemyDosIniciar = false;
 
-
+	int bp = 0;
 
 	bool juego = true;
 	while (juego == true)
@@ -85,12 +86,11 @@ int main() {
 		playerUno.setApodo(eleccion);
 		cout << "Bienvenido " << playerUno.getApodo() << "\n";
 
-
 		///////////////////////////////////////////////////////////////////////////////////////////////// ELECCION BARCO
 		bool cat = true;
 		while (cat == true)
 		{
-			cout << "Escoje una categoria: \n \t 1.- Small Barcos. \n \t 2.- Medium Barcos. \n \t 3.- Big Barcos. \n \t 0.- Salir del Juego.\n Opcion: ";
+			cout << "Escoje una categoria: \n \t 1.- Small Barcos. \n \t 2.- Medium Barcos. \n \t 3.- Big Barcos. \n Opcion: ";
 			cin >> eleccion;
 
 			if (eleccion == "1")
@@ -112,15 +112,17 @@ int main() {
 
 							if (eleccion == "s" || eleccion == "S" || eleccion == "si" || eleccion == "SI") {
 								playerUno.setNombreBarco("Kayuco");
-								playerUno.setVelocidad(2);
+								playerUno.setVelocidad(0);
 								playerUno.setVidas(1);
 								playerUno.setAtk(false);
 								playerUno.setNombreAtk("atkKayuco");
 								playerUno.setDescripcionAtk("Lanza cadaveres al agua");
 
+								playerUnoIniciar = true;
 								cat = false;
 								tipBarco = false;
 								escbarco = false;
+								bp = 1;
 							}
 							else if (eleccion == "n" || eleccion == "N" || eleccion == "no" || eleccion == "NO") {
 								tipBarco = false;
@@ -131,26 +133,28 @@ int main() {
 							}
 						}
 					}
-					if (eleccion == "2")
+					else if (eleccion == "2")
 					{
 						bool tipBarco = true;
 						while (tipBarco == true)
 						{
-							cout << "\n Balsa \n \t Descripcion: Embarcacion simple echa de troncos unidos entre si";																					//editar
+							cout << "\n Balsa \n \t Descripcion: Embarcacion simple echa de troncos unidos entre si";
 							cout << "\n Escojes este tipo de barco? s/n \n Respuesta: ";
 							cin >> eleccion;
 
 							if (eleccion == "s" || eleccion == "S" || eleccion == "si" || eleccion == "SI") {
-								playerUno.setNombreBarco("Balsa");																				//EDITAR DESDE AQUI
-								playerUno.setVelocidad(2);
+								playerUno.setNombreBarco("Balsa");
+								playerUno.setVelocidad(0);
 								playerUno.setVidas(1);
 								playerUno.setAtk(false);
 								playerUno.setNombreAtk("atkBalsa");
-								playerUno.setDescripcionAtk("Lanzar cantidades ingentes de cocos");																			//EDITAR HASTA AQUI
+								playerUno.setDescripcionAtk("Lanzar cantidades ingentes de cocos");
 
+								playerUnoIniciar = true;
 								cat = false;
 								tipBarco = false;
 								escbarco = false;
+								bp = 1;
 							}
 							else if (eleccion == "n" || eleccion == "N" || eleccion == "no" || eleccion == "NO") {
 								tipBarco = false;
@@ -161,26 +165,28 @@ int main() {
 							}
 						}
 					}
-					if (eleccion == "3")
+					else if (eleccion == "3")
 					{
 						bool tipBarco = true;
 						while (tipBarco == true)
 						{
-							cout << "\n Gondola \n \t Descripcion: Embarcacion tradicional de venecia, muy conocida por por sus elegante diseños y su usos por la ciudad";																					//editar
+							cout << "\n Gondola \n \t Descripcion: Embarcacion tradicional de venecia, muy conocida por por sus elegante diseños y su usos por la ciudad";
 							cout << "\n Escojes este tipo de barco? s/n \n Respuesta: ";
 							cin >> eleccion;
 
 							if (eleccion == "s" || eleccion == "S" || eleccion == "si" || eleccion == "SI") {
-								playerUno.setNombreBarco("Gondola");																				//EDITAR DESDE AQUI
-								playerUno.setVelocidad(2);
+								playerUno.setNombreBarco("Gondola");
+								playerUno.setVelocidad(0);
 								playerUno.setVidas(1);
 								playerUno.setAtk(false);
 								playerUno.setNombreAtk("atkGondola");
-								playerUno.setDescripcionAtk("Toca el acordeon y a media cancion lanza su acordeon");																			//EDITAR HASTA AQUI
+								playerUno.setDescripcionAtk("Toca el acordeon y a media cancion lanza su acordeon");
 
+								playerUnoIniciar = true;
 								cat = false;
 								tipBarco = false;
 								escbarco = false;
+								bp = 1;
 							}
 							else if (eleccion == "n" || eleccion == "N" || eleccion == "no" || eleccion == "NO") {
 								tipBarco = false;
@@ -191,44 +197,272 @@ int main() {
 							}
 						}
 					}
-					if (eleccion == "0")
+					else if (eleccion == "0")
 					{
-						cout << "\ndentro del 0";//borrar
-
+						escbarco = false;
 					}
-					//else
-					//{
-					//	cout << "Eleccion incorrecta";
-					//}
+					else
+					{
+						cout << "\nEleccion incorrecta";
+					}
 				}
 			}
-			if (eleccion == "2")
+			else if (eleccion == "2")
 			{
+				bool escbarco = true;
+				while (escbarco == true)
+				{
+					cout << "Tienes 3 barcos en esta categoria cual quieres escojer?";
+					cout << "\n \t 1.- Narcolancha. \n \t 2.- Galera. \n \t 3.- Drakkar. \n \t 0.- Menu anterior. \n Opcion: ";
+					cin >> eleccion;
+					if (eleccion == "1")
+					{
+						bool tipBarco = true;
+						while (tipBarco == true)
+						{
+							cout << "\nNarcolancha \n \t Descripcion: ";
+							cout << "\n Escojes este tipo de barco? s/n \n Respuesta: Embarcacion inchable usada originalmente para rescate, buceo y exploracion de aguas costeras, ahora adaptada para trabajar!";
+							cin >> eleccion;
 
+							if (eleccion == "s" || eleccion == "S" || eleccion == "si" || eleccion == "SI") {
+								playerUno.setNombreBarco("Narcolancha");
+								playerUno.setVelocidad(0);
+								playerUno.setVidas(1);
+								playerUno.setAtk(false);
+								playerUno.setNombreAtk("atkNarcolancha");
+								playerUno.setDescripcionAtk("Lanza fardos de droga al mar");
+
+								playerUnoIniciar = true;
+								cat = false;
+								tipBarco = false;
+								escbarco = false;
+								bp = 2;
+							}
+							else if (eleccion == "n" || eleccion == "N" || eleccion == "no" || eleccion == "NO") {
+								tipBarco = false;
+							}
+							else if (!(eleccion == "s" || eleccion == "S" || eleccion == "si" || eleccion == "SI" ||
+								eleccion == "n" || eleccion == "N" || eleccion == "no" || eleccion == "NO")) {
+								cout << "\nEleccion incorrecta";
+							}
+						}
+					}
+					else if (eleccion == "2")
+					{
+						bool tipBarco = true;
+						while (tipBarco == true)
+						{
+							cout << "\n Galera \n \t Descripcion: Embarcacion utilizada en la edad media, equipada con muchas filas de remos y catapultas";
+							cout << "\n Escojes este tipo de barco? s/n \n Respuesta: ";
+							cin >> eleccion;
+
+							if (eleccion == "s" || eleccion == "S" || eleccion == "si" || eleccion == "SI") {
+								playerUno.setNombreBarco("Galera");
+								playerUno.setVelocidad(0);
+								playerUno.setVidas(1);
+								playerUno.setAtk(false);
+								playerUno.setNombreAtk("atkGalera");
+								playerUno.setDescripcionAtk("Catapultar a los esclavos cansados");
+
+								playerUnoIniciar = true;
+								cat = false;
+								tipBarco = false;
+								escbarco = false;
+								bp = 2;
+							}
+							else if (eleccion == "n" || eleccion == "N" || eleccion == "no" || eleccion == "NO") {
+								tipBarco = false;
+							}
+							else if (!(eleccion == "s" || eleccion == "S" || eleccion == "si" || eleccion == "SI" ||
+								eleccion == "n" || eleccion == "N" || eleccion == "no" || eleccion == "NO")) {
+								cout << "\nEleccion incorrecta";
+							}
+						}
+					}
+					else if (eleccion == "3")
+					{
+						bool tipBarco = true;
+						while (tipBarco == true)
+						{
+							cout << "\n Drakkar \n \t Descripcion: Envarcacion vkinga utilizada por guerreros durante la edad media";
+							cout << "\n Escojes este tipo de barco? s/n \n Respuesta: ";
+							cin >> eleccion;
+
+							if (eleccion == "s" || eleccion == "S" || eleccion == "si" || eleccion == "SI") {
+								playerUno.setNombreBarco("Drakkar");
+								playerUno.setVelocidad(0);
+								playerUno.setVidas(1);
+								playerUno.setAtk(false);
+								playerUno.setNombreAtk("atkDrakkar");
+								playerUno.setDescripcionAtk("Vikingos ebrios de hidromiel lanzan hachas a diestro y siniestro");
+
+								playerUnoIniciar = true;
+								cat = false;
+								tipBarco = false;
+								escbarco = false;
+								bp = 2;
+							}
+							else if (eleccion == "n" || eleccion == "N" || eleccion == "no" || eleccion == "NO") {
+								tipBarco = false;
+							}
+							else if (!(eleccion == "s" || eleccion == "S" || eleccion == "si" || eleccion == "SI" ||
+								eleccion == "n" || eleccion == "N" || eleccion == "no" || eleccion == "NO")) {
+								cout << "\nEleccion incorrecta";
+							}
+						}
+					}
+					else if (eleccion == "0")
+					{
+						escbarco = false;
+					}
+					else
+					{
+						cout << "\nEleccion incorrecta";
+					}
+				}
+			}
+			else if (eleccion == "3")
+			{
+				bool escbarco = true;
+				while (escbarco == true)
+				{
+					cout << "Tienes 3 barcos en esta categoria cual quieres escojer?";
+					cout << "\n \t 1.- Buque mercante. \n \t 2.- Crucero de lujo. \n \t 3.- Buque de investigacion. \n \t 0.- Menu anterior. \n Opcion: ";
+					cin >> eleccion;
+					if (eleccion == "1")
+					{
+						bool tipBarco = true;
+						while (tipBarco == true)
+						{
+							cout << "\n Buque mercante \n \t Descripcion: Utilizado para el transporte de mercancias";
+							cout << "\n Escojes este tipo de barco? s/n \n Respuesta: ";
+							cin >> eleccion;
+
+							if (eleccion == "s" || eleccion == "S" || eleccion == "si" || eleccion == "SI") {
+								playerUno.setNombreBarco("Buque mercante");
+								playerUno.setVelocidad(0);
+								playerUno.setVidas(1);
+								playerUno.setAtk(false);
+								playerUno.setNombreAtk("atkBuque mercante");
+								playerUno.setDescripcionAtk("Lanzamiento de mercancias procedentes de china");
+
+								playerUnoIniciar = true;
+								cat = false;
+								tipBarco = false;
+								escbarco = false;
+								bp = 3;
+							}
+							else if (eleccion == "n" || eleccion == "N" || eleccion == "no" || eleccion == "NO") {
+								tipBarco = false;
+							}
+							else if (!(eleccion == "s" || eleccion == "S" || eleccion == "si" || eleccion == "SI" ||
+								eleccion == "n" || eleccion == "N" || eleccion == "no" || eleccion == "NO")) {
+								cout << "\nEleccion incorrecta";
+							}
+						}
+					}
+					else if (eleccion == "2")
+					{
+						bool tipBarco = true;
+						while (tipBarco == true)
+						{
+							cout << "\n Crucero de lujo \n \t Descripcion: ";
+							cout << "\n Escojes este tipo de barco? s/n \n Respuesta: ";
+							cin >> eleccion;
+
+							if (eleccion == "s" || eleccion == "S" || eleccion == "si" || eleccion == "SI") {
+								playerUno.setNombreBarco("Crucero de lujo");
+								playerUno.setVelocidad(0);
+								playerUno.setVidas(1);
+								playerUno.setAtk(false);
+								playerUno.setNombreAtk("atkCrucero de lujo");
+								playerUno.setDescripcionAtk("");
+
+								playerUnoIniciar = true;
+								cat = false;
+								tipBarco = false;
+								escbarco = false;
+								bp = 3;
+							}
+							else if (eleccion == "n" || eleccion == "N" || eleccion == "no" || eleccion == "NO") {
+								tipBarco = false;
+							}
+							else if (!(eleccion == "s" || eleccion == "S" || eleccion == "si" || eleccion == "SI" ||
+								eleccion == "n" || eleccion == "N" || eleccion == "no" || eleccion == "NO")) {
+								cout << "\nEleccion incorrecta";
+							}
+						}
+					}
+					else if (eleccion == "3")
+					{
+						bool tipBarco = true;
+						while (tipBarco == true)
+						{
+							cout << "\n  \n \t Descripcion: ";
+							cout << "\n Escojes este tipo de barco? s/n \n Respuesta: ";
+							cin >> eleccion;
+
+							if (eleccion == "s" || eleccion == "S" || eleccion == "si" || eleccion == "SI") {
+								playerUno.setNombreBarco("");
+								playerUno.setVelocidad(0);
+								playerUno.setVidas(1);
+								playerUno.setAtk(false);
+								playerUno.setNombreAtk("atk");
+								playerUno.setDescripcionAtk("");
+
+								playerUnoIniciar = true;
+								cat = false;
+								tipBarco = false;
+								escbarco = false;
+								bp = 3;
+							}
+							else if (eleccion == "n" || eleccion == "N" || eleccion == "no" || eleccion == "NO") {
+								tipBarco = false;
+							}
+							else if (!(eleccion == "s" || eleccion == "S" || eleccion == "si" || eleccion == "SI" ||
+								eleccion == "n" || eleccion == "N" || eleccion == "no" || eleccion == "NO")) {
+								cout << "\nEleccion incorrecta";
+							}
+						}
+					}
+					else if (eleccion == "0")
+					{
+						escbarco = false;
+					}
+					else
+					{
+						cout << "\nEleccion incorrecta";
+					}
+				}
+			}
+			else
+			{
+				cout << "\nEleccion incorrecta";
+			}
+		}
+		if (playerUnoIniciar == true)
+		{
+			// enemigos escojen barco al azar, de la misa categoria donde lo escojio el player
+			string nombre;
+			bool atk;
+			switch (bp)
+			{
+			case 1:
+				// rand para generar un numero del 1 al 3
+				// otro switch con todas las opciones
+				// dar datos para el enemigo 
+				return nombre, atk;
+
+			case 2:
+
+			case 3:
 
 			}
-			if (eleccion == "3")
-			{
 
 
-			}
-			if (eleccion == "0")
-			{
-
-			}
-			//else
-			//{
-			//	cout << "Eleccion incorrecta";
-			//}
 		}
 	}
 }
-
-
-
-
-
-
 ////							nombre vel. atk. vida. nombre atk
 //Barcos barcoSmall1 = Barcos("Kayuco", 5, false, 1, "atkKayuco");
 //Barcos barcoSmall2 = Barcos("Balsa", 5, false, 1, "atkBalsa");
